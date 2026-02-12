c6300d88dcbc5896aa165d2d81e5bb523828e713
tewolde marie <tewoldemarie6@gmail.com>
2026-02-12 22:50:05 +0300

    StreaksScreen is started and ongoing

---
+++ app/(tabs)/streaks.tsx
@@ -0,0 +1,32 @@
+import { useAuth } from "@/lib/auth-context";
+// import { Habit, HabitCompletion } from "@/types/database.type";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Query } from "react-native-appwrite";
+import { ScrollView } from "react-native-gesture-handler";
+import { Card, Text } from "react-native-paper";
+export default function StreaksScreen() {
+
+    return (
+<View style={styles.container}>
+      <Text style={styles.title} variant="headlineSmall">
+        {" "}
+        Habit Streaks
+      </Text>
+            
+    </View>
+)
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+        padding: 16,
+    },
+    title: {
+        fontWeight: "bold",
+        marginBottom: 16,
+    },
+});
